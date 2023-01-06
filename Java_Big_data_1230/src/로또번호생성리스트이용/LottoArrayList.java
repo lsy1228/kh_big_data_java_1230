5fa3f6231bca9706dda6753eb9a0d73307d149b6
lsy1228 <258735@naver.com>
2023-01-06 14:19:54 +0900

    로또번호생성리스트이용

---
+++ Java_Big_data_1230/src/로또번호생성리스트이용/LottoArrayList.java
@@ -0,0 +1,20 @@
+package 로또번호생성리스트이용;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(); //개수 지정 필요없음
+        int tmp; //로또번호를 임시로 저장하는 변수
+        while(true) {
+            tmp = (int)((Math.random() * 45) + 1);
+            if(!list.contains(tmp))
+                list.add(tmp);
+            if(list.size() == 6)
+                break;
+        }
+        for(Integer e : list)
+            System.out.print(e + " ");
+    }
+}
