8152a14a1ce4a9ce00c274fc45fa962004170109
lsy1228 <258735@naver.com>
2023-01-12 09:09:18 +0900

    메소드연습

--- Java_Big_data_1230/src/메소드연습/ClassMethod.java
+++ Java_Big_data_1230/src/메소드연습/ClassMethod.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
 //접근제한자 반환타입 메소드이름 (매개변수 목록) { 구현부 ; return }
 public class ClassMethod {
     public static void main(String[] args) {
-        Sample sample = new Sample(); //클래스 내에 생성자가 없으면 기본 생성자가 호출됨
+        Sample sample = new Sample(); //생성자 호출 시 전달하는 매개변수가 없음
+        //Sample sample = new Sample(); //클래스 내에 생성자가 없으면 기본 생성자가 호출됨
         Scanner sc = new Scanner(System.in);
         System.out.print("정수값 입력 : ");
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int rst = sample.sum(n1, n2); //sum()은 매개변수와 반환값이 있는 메소드
         System.out.println(rst);
-        sample.say();
+        sample.say(); //say() 메소드를 호출하기 위해서는 클래스를 객체로 만들고 객체의 메소드로 접근
         System.out.println(sample.sayStr());
+        int rst2 = sample.sum(1,2,3,4,5);
+        System.out.println(rst2);
     }
 }
