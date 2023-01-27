b41d33e5d2a92e2f6d6b6941279ad61700090f75
lsy1228 <258735@naver.com>
2023-01-27 14:32:08 +0900

    Class 클래스

---
+++ Java_Big_data_1230/src/Class클래스/ClassTestEx.java
@@ -0,0 +1,17 @@
+package Class클래스;
+//Class : 클래스의 패키지 이름이나 클래스 이름등의 정보를 가져옴
+public class ClassTestEx {
+    public static void main(String[] args) {
+        String s = new String();
+        Class c = s.getClass(); //해당 클래스의 정보를 가져옴
+        System.out.println(c);
+
+        Person person = new Person();
+        Class pCls = person.getClass();
+        System.out.println(pCls.getSimpleName());
+        System.out.println(pCls.getClass());
+    }
+}
+class Person {
+
+}
