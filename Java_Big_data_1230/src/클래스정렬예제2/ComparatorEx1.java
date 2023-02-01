0a2aec16e3a85fada3644e964d7fff85c1bb16a2
lsy1228 <258735@naver.com>
2023-02-01 13:33:36 +0900

    2/1 클래스정렬 예제2

---
+++ Java_Big_data_1230/src/클래스정렬예제2/ComparatorEx1.java
@@ -0,0 +1,17 @@
+package 클래스정렬예제2;
+import java.util.TreeSet;
+
+//Comparator 역시 정렬을 구현하는데 사용하는 인터페이스
+public class ComparatorEx1 {
+    public static void main(String[] args) {
+        TreeSet<Fruit> ts = new TreeSet<>(new DescendComp());
+        ts.add(new Fruit("포도", 3000));
+        ts.add(new Fruit("수박", 12000));
+        ts.add(new Fruit("딸기", 16000));
+        ts.add(new Fruit("키위", 16000));
+
+        for(Fruit e : ts) {
+            System.out.println("이름 : " + e.name + ", 가격 : " + e.price);
+        }
+    }
+}
