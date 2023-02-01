7acabc720bbe85c3223d82ac1e6986d9b2ad7ba0
lsy1228 <258735@naver.com>
2023-02-01 13:57:32 +0900

    로또번호생성리스트이용

--- Java_Big_data_1230/src/로또번호생성리스트이용/LottoArrayList.java
+++ Java_Big_data_1230/src/로또번호생성리스트이용/LottoArrayList.java
@@ -1,8 +1,9 @@
 package 로또번호생성리스트이용;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
+//리스트 타입 => HashSet을 이용해 중복 제거로 변경하기
 public class LottoArrayList {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(); //개수 지정 필요없음
@@ -16,5 +17,15 @@ public class LottoArrayList {
         }
         for(Integer e : list)
             System.out.print(e + " ");
+
+        //HashSet 이용
+        HashSet<Integer> lotto = new HashSet<>();
+        while(true) {
+            tmp = (int)((Math.random() * 45) + 1);
+            lotto.add(tmp);
+            if(lotto.size() == 6)
+                break;
+        }
+        System.out.println(lotto);
     }
 }
