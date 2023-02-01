162a12675e1e001a37fe7739f99918e86603c2b5
lsy1228 <258735@naver.com>
2023-02-01 13:58:42 +0900

    2/1 클래스정렬 예제2

--- Java_Big_data_1230/src/클래스정렬예제2/ComparatorEx1.java
+++ Java_Big_data_1230/src/클래스정렬예제2/ComparatorEx1.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 //Comparator 역시 정렬을 구현하는데 사용하는 인터페이스
 public class ComparatorEx1 {
     public static void main(String[] args) {
-        TreeSet<Fruit> ts = new TreeSet<>(new DescendComp()); //정렬 조건을 객체에 넣어줌
+        TreeSet<Fruit> ts = new TreeSet<>(new DescendComp());  //정렬 조건을 객체에 넣어줌
         ts.add(new Fruit("포도", 3000));
         ts.add(new Fruit("수박", 12000));
         ts.add(new Fruit("딸기", 16000));
