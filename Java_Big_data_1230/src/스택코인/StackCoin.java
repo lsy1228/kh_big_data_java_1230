a7038879131d87d1dd2f2040479320e1c89f1275
lsy1228 <258735@naver.com>
2023-02-02 14:01:04 +0900

    2/2 스택 코인

---
+++ Java_Big_data_1230/src/스택코인/StackCoin.java
@@ -0,0 +1,27 @@
+package 스택코인;
+import java.util.Stack;
+
+public class StackCoin {
+    public static void main(String[] args) {
+        Stack<Coin> coinBox = new Stack<>();
+        coinBox.push(new Coin(100));
+        coinBox.push(new Coin(50));
+        coinBox.push(new Coin(500));
+        coinBox.push(new Coin(10));
+        while(!coinBox.isEmpty()) {
+            Coin coin = coinBox.pop();
+            System.out.println("꺼내온 동전 : " + coin.getValue());
+        }
+    }
+}
+class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
