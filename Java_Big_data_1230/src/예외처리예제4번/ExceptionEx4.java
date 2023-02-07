c075a6389ebff66ef6deba8ef8d12ed61471b54d
lsy1228 <258735@naver.com>
2023-02-07 14:05:36 +0900

    2/7 예외처리예제4번

---
+++ Java_Big_data_1230/src/예외처리예제4번/ExceptionEx4.java
@@ -0,0 +1,24 @@
+package 예외처리예제4번;
+
+import java.util.Scanner;
+
+//ArithmeticException : 연산 오류
+public class ExceptionEx4 {
+    public static void main(String[] args) {
+        int rst = 0;
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("나눗셈 입력 : ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            try {
+                rst = a / b;
+            } catch (ArithmeticException e) {
+                System.out.println("0으로 나눌 수 없습니다. 다시 입력하세요");
+                continue;
+            }
+            System.out.println("결과 : " + rst);
+            break;
+        }
+    }
+}
