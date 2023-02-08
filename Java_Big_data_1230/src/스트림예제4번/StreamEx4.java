750899b92fd72f70ca5b5267e180aeb9d51b2856
lsy1228 <258735@naver.com>
2023-02-08 14:09:10 +0900

    2/8 스트림 예제4번

---
+++ Java_Big_data_1230/src/스트림예제4번/StreamEx4.java
@@ -0,0 +1,17 @@
+package 스트림예제4번;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+//파일로부터 스트림 얻기
+public class StreamEx4 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src/스트림예제4번/chicken.txt");
+        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+        stream.forEach(System.out::println);
+        System.out.println();
+    }
+}
