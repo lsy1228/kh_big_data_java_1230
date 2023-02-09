c1f3f21b94caeb1ee25a2989d29d7104548eba14
lsy1228 <258735@naver.com>
2023-02-09 15:00:24 +0900

    2/9 입출력스트림예제5번

---
+++ Java_Big_data_1230/src/입출력스트림예제5번/InOutStreamEx5.java
@@ -0,0 +1,27 @@
+package 입출력스트림예제5번;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class InOutStreamEx5 {
+    public static void main(String[] args) throws IOException {
+        String originalFileName = "src/입출력스트림예제5번/cat_0209.jpg";
+        String targetFileName = "d:/clone_0209.jpg";
+
+        FileInputStream fis = new FileInputStream(originalFileName);
+        FileOutputStream fos = new FileOutputStream(targetFileName);
+
+        int readByteNo;
+        int cnt = 0;
+        byte[] buffer = new byte[100];
+        while((readByteNo = fis.read(buffer)) != -1) {
+            fos.write(buffer, 0, readByteNo);
+            cnt++;
+        }
+        fos.flush();
+        fos.close();
+        fis.close();
+        System.out.println("복사 완료, 버퍼 읽기 횟수 : " + cnt);
+    }
+}
